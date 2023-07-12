e138e07c20d18d2dd37b9a32f53e63d3a4c8386a
alexlee <lee.alex@student.greenriver.edu>
2023-07-11 17:27:29 -0700

    STarted HelloWorld

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python
-# Created 
+# Created by Alex on July 11, 2023
+
+#Get name
+username = input("What is your name? ")
+
+print("Hello " + username)
+#print("Hello {0}".format(username))
